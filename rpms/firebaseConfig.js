c17275ba4110a147a4e19be9cac115ed1d2f44d6
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-07 00:41:00 -0500

    Update firebaseConfig.js

--- rpms/firebaseConfig.js
+++ rpms/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-KzhsOiwgLMoWFFWvWxi-J1BIFvBbMgs",
-  authDomain: "rpms-hr.firebaseapp.com",
-  databaseURL: "https://rpms-hr-default-rtdb.firebaseio.com",
-  projectId: "rpms-hr",
-  storageBucket: "rpms-hr.firebasestorage.app",
-  messagingSenderId: "575342774385",
-  appId: "1:575342774385:web:b0b081b42ddb54b47ff3d0",
+  apiKey: "***********************",
+  authDomain: "***********************",
+  databaseURL: "***********************",
+  projectId: "***********************",
+  storageBucket: "***********************",
+  messagingSenderId: "***********************",
+  appId: "***********************",
 };
 
 const app = initializeApp(firebaseConfig);
